8fb456fb97fbf9d192fdadbe7803e037d6d75543
Thomas Mielke <thomas@mielke.software>
2023-09-08 17:02:43 +0200

    released under the GNU Affero General Public License v3

--- GeorgaMobileDatabase/Role.cs
+++ GeorgaMobileDatabase/Role.cs
@@ -1,3 +1,22 @@
+/* GeoRGA Mobile Client -- a multi-platform mobile app for the
+ * Geographic Resouce and Group Allocation project (https://georga.app/)
+ * 
+ * Copyright (C) 2023 Thomas Mielke D8AE2CE41CB1D1A61087165B95DC1917252AD305 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 using GeorgaMobileDatabase.Model;
 using System.Collections.Generic;
 using System.Threading.Tasks;
